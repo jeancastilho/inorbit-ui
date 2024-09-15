6f89f3870a002d525e24ed4c34948884c108319f
Jean <jean@smart4sistemas.com>
2024-09-15 13:13:01 -0300

    Criando as telas de consulta e cadastro de metas

--- src/app.tsx
+++ src/app.tsx
@@ -1,25 +1,18 @@
-import {Plus} from 'lucide-react'
 
-import logo from './assets/logo-in-orbit.svg'
-import letsStart from './assets/lets-start-illustration.svg'
-import { Button } from './components/ui/button'
-
+import {
+  Dialog, DialogTrigger
+} from './components/ui/dialog'
+import { CreateGoal } from './components/create-goal'
+import { EmptyGoals } from './components/empty-goals'
+import { Summary } from './components/summary'
 
 export function App() {
   return (
-    <div className="h-screen flex flex-col items-center justify-center gap-8">
-      <img src={logo} alt="in.orbit" />
-      <img src={letsStart} alt="in.orbit" />
-
-      <p className='text-zinc-300 leading-relaxed max-w-80 text-center'> 
-        Você ainda não cadastrou nenhuma meta, que tal cadastrar uma agora mesmo?
-      </p>
-
-      <Button> 
-        <Plus className='size-4'  />
-        Cadastrar Meta
-      </Button>
-
-    </div>
+    <Dialog>
+   {/* <EmptyGoals /> */} 
+      
+      <Summary />
+     <CreateGoal />
+    </Dialog>
   )
 }
